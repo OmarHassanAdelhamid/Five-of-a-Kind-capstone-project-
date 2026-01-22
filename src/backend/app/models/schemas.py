fe8156da4625fe73e3f0abc98c7892a8776dc530
Khalid Farag <faragk1@mcmaster.ca>
2026-01-22 17:28:21 -0500

    Added  update layer request

--- src/backend/app/models/schemas.py
+++ src/backend/app/models/schemas.py
@@ -1,4 +1,4 @@
-
+from typing import List, Optional
 from pydantic import BaseModel
 
 
@@ -6,3 +6,10 @@ class VoxelizeRequest(BaseModel):
     stl_filename: str
     voxel_size: float
     project_name: str
+
+class UpdateLayerRequest(BaseModel):
+    project_name: str
+    layer_value: float 
+    voxels: List[List[float]]  
+    voxel_size: Optional[float] = None
+    axis: Optional[str] = "z" 
